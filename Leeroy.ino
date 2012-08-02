05c9e140e603462ce9f04e97896589e4aca6d8dc
Bjarte Stien Karlsen <bjarte@bjartek.org>
2012-08-02 22:15:30 +0200

    fixing the code, creating schema and making readme

--- Leeroy.ino
+++ Leeroy.ino
@@ -10,34 +10,27 @@
 
 #include <SPI.h>
 #include <Ethernet.h>
-#include <string.h>
 
 byte mac[] = {  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
 
 EthernetClient client;
 
+//For holding the title of the entry
+const int MY_BUFFER_SIZE = 100;
+char buffer [MY_BUFFER_SIZE+1]; 
+
 //variables for polling
 unsigned long lastConnectionTime = 0;          // last time you connected to the server, in milliseconds
 boolean lastConnected = false;                 // state of the connection last time through the main loop
-const unsigned long postingInterval = 60*1000;  // delay between updates, in milliseconds
+const unsigned long postingInterval = 30000;  // delay between updates, in milliseconds 1 minute
 
-
-// Max string length may have to be adjusted depending on data to be extracted
-#define MAX_STRING_LEN  30
-
-// Setup vars for text parsing
-char tagStr[MAX_STRING_LEN] = "";
-char dataStr[MAX_STRING_LEN] = "";
-char tmpStr[MAX_STRING_LEN] = "";
-char endTag[3] = {'<', '/', '\0'};
-int len;
-
-// Flags to differentiate XML tags from document elements (ie. data)
-boolean tagFlag = false;
-boolean dataFlag = false;
-boolean entryFalg = false;
+//status of light
+String light = "";
 
 void setup() {
+  pinMode(3, OUTPUT); 
+  pinMode(5, OUTPUT);  
+  
   // start serial port:
   Serial.begin(9600);
   // give the ethernet module time to boot up:
@@ -47,6 +40,10 @@ void setup() {
   // print the Ethernet board/shield's IP address:
   Serial.print("My IP address: ");
   Serial.println(Ethernet.localIP());
+   // for debugging
+  Serial.print("Polling every ");
+  Serial.print(postingInterval);
+  Serial.println(" miliseconds");
 }
 
 void loop() {
@@ -54,7 +51,7 @@ void loop() {
   // send it out the serial port.  This is for debugging
   // purposes only:
   if (client.available()) {
-       serialEvent();
+    serialEvent();
   }
 
   // if there's no net connection, but there was one last time
@@ -78,7 +75,6 @@ void loop() {
 // this method makes a HTTP connection to the server:
 void httpRequest() {
   //reset the entry flag when we make a new conncetoin
-  entryFlag = false;
   // if there's a successful connection:
   if (client.connect("dl.dropbox.com", 80)) {
     Serial.println("connecting...");
@@ -86,7 +82,7 @@ void httpRequest() {
     //TODO: configure somewhere else?
     client.println("GET /u/667447/test.rss HTTP/1.1");
     client.println("Host: dl.dropbox.com");
-    client.println("User-Agent: Leerot: Arduino CI lamt");
+    client.println("User-Agent: Leeroy Arduino CI lamp");
     client.println("Connection: close");
     client.println();
 
@@ -101,133 +97,41 @@ void httpRequest() {
   }
 }
 
-
-// Process each char from web
 void serialEvent() {
-
-   // Read a char
-   char inChar = client.read();
-   //Serial.print(".");
-  
-   if (inChar == '<') {
-      addChar(inChar, tmpStr);
-      tagFlag = true;
-      dataFlag = false;
-
-   } else if (inChar == '>') {
-      addChar(inChar, tmpStr);
-
-      if (tagFlag) {      
-         strncpy(tagStr, tmpStr, strlen(tmpStr)+1);
-      }
-
-      // Clear tmp
-      clearStr(tmpStr);
-
-      tagFlag = false;
-      dataFlag = true;      
-      
-   } else if (inChar != 10) {
-      if (tagFlag) {
-         // Add tag char to string
-         addChar(inChar, tmpStr);
-
-         // Check for </XML> end tag, ignore it
-         if ( tagFlag && strcmp(tmpStr, endTag) == 0 ) {
-            clearStr(tmpStr);
-            tagFlag = false;
-            dataFlag = false;
-         }
-      }
-      
-      if (dataFlag) {
-         // Add data char to string
-         addChar(inChar, dataStr);
-      }
-   }  
-  
-   // If a LF, process the line
-   if (inChar == 10 ) {
-
-/*
-      Serial.print("tagStr: ");
-      Serial.println(tagStr);
-      Serial.print("dataStr: ");
-      Serial.println(dataStr);
-*/
-
-      // Set a flag if we have passed a entry tag.
-      if (matchTag("<entry>")) {
-        entryFlag = true;
-        Serial.println("entry found");
-      }
-      
-      if(entryFlag && matchTag("<title>")) {
-        Serial.println(dataStr);
-        Serial.println(strpos(dataStr, "(stable)"));
-      }
-
-      // Clear all strings
-      clearStr(tmpStr);
-      clearStr(tagStr);
-      clearStr(dataStr);
-
-      // Clear Flags
-      tagFlag = false;
-      dataFlag = false;
-   }
-}
-
-/////////////////////
-// Other Functions //
-/////////////////////
-
-// Function to clear a string
-void clearStr (char* str) {
-   int len = strlen(str);
-   for (int c = 0; c < len; c++) {
-      str[c] = 0;
-   }
-}
-
-//Function to add a char to a string and check its length
-void addChar (char ch, char* str) {
-   char *tagMsg  = "<TRUNCATED_TAG>";
-   char *dataMsg = "-TRUNCATED_DATA-";
-
-   // Check the max size of the string to make sure it doesn't grow too
-   // big.  If string is beyond MAX_STRING_LEN assume it is unimportant
-   // and replace it with a warning message.
-   if (strlen(str) > MAX_STRING_LEN - 2) {
-      if (tagFlag) {
-         clearStr(tagStr);
-         strcpy(tagStr,tagMsg);
-      }
-      if (dataFlag) {
-         clearStr(dataStr);
-         strcpy(dataStr,dataMsg);
-      }
-
-      // Clear the temp buffer and flags to stop current processing
-      clearStr(tmpStr);
-      tagFlag = false;
-      dataFlag = false;
-
-   } else {
-      // Add char to string
-      str[strlen(str)] = ch;
-   }
-}
-
-// Function to check the current tag for a specific string
-boolean matchTag (char* searchTag) {
-   if ( strcmp(tagStr, searchTag) == 0 ) {
-      return true;
-   } else {
-      return false;
-   }
-}
-
  
+  if(client.find("<entry>")) {
+    if(client.find("<title>")) {
+      if(client.readBytesUntil('<', buffer, MY_BUFFER_SIZE)) {
+        if(String(buffer).indexOf("(stable)") != -1) {
+          if(light != "GREEN") {
+            green();
+          }
+        } else {
+          if(light != "RED") {
+            red();
+          }
+        } 
+        
+        client.stop();
+        client.flush();  
+      }
+    }
+  }
+}
+
+
+void red() {
+  digitalWrite(3, LOW); 
+  digitalWrite(5, HIGH); 
+  light = "RED";
+  Serial.println("red");
+}
+
+void green() {
+  digitalWrite(5, LOW);  
+  digitalWrite(3, HIGH);
+  light = "GREEN";
+  Serial.println("green");
+}
 
 
